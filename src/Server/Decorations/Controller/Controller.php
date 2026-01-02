a8a978ede91cf8a444196d616752b7124a72e439
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2026-01-02 14:06:48 -0300

    Atualizar componentes

--- src/Server/Decorations/Controller/Controller.php
+++ src/Server/Decorations/Controller/Controller.php
@@ -2,14 +2,41 @@
 
 namespace Websyspro\Core\Server\Decorations\Controller;
 
-use Websyspro\Enums\ControllerType;
+use Websyspro\Core\Server\Enums\ControllerType;
 use Attribute;
 
+
+/**
+ * Marks a class as a Controller with a URI prefix.
+ *
+ * This attribute is applied to a controller class to indicate that
+ * it is a web API controller and to define a common URI prefix
+ * for all endpoints within the class.
+ *
+ * Example usage:
+ *   #[Controller("/users")]
+ *   class UserController { ... }
+ *
+ * All endpoints inside UserController will be prefixed with "/users".
+ */
 #[Attribute(Attribute::TARGET_CLASS)]
 class Controller
 {
+  /**
+   * Specifies the type of this controller.
+   *
+   * ControllerType::Controller indicates that this is a main controller class,
+   * rather than middleware or a parameter.
+   *
+   * @var ControllerType
+   */  
   public ControllerType $controllerType = ControllerType::Controller;
 
+  /**
+   * Constructor for the Controller attribute.
+   *
+   * @param string $prefix The URI prefix for all endpoints in this controller.
+   */  
   public function __construct(
     public string $prefix
   ){}
