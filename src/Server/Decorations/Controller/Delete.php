b63edb76c46862c3ad7c2f1cccd5802ceba9c3f2
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2026-01-02 14:08:17 -0300

    Atualizar componentes

--- src/Server/Decorations/Controller/Delete.php
+++ src/Server/Decorations/Controller/Delete.php
@@ -2,13 +2,42 @@
 
 namespace Websyspro\Core\Server\Decorations\Controller;
 
-use Websyspro\Enums\ControllerType;
-use Websyspro\Enums\MethodType;
 use Attribute;
+use Websyspro\Core\Server\Enums\ControllerType;
+use Websyspro\Core\Server\Enums\HttpMethod;
 
+
+/**
+ * Marks a controller method as handling HTTP DELETE requests.
+ *
+ * This attribute is applied to methods to indicate that they should
+ * respond to DELETE requests on the specified URI.
+ *
+ * Example usage:
+ *   #[Delete("/users/:id")]
+ *   public function deleteUser(Request $request) { ... }
+ *
+ * Extends AbstractEndpoint, inheriting properties such as `uri`.
+ */
 #[Attribute(Attribute::TARGET_METHOD)]
 class Delete extends AbstractEndpoint
 {
-  public MethodType $methodType = MethodType::DELETE;
+  /**
+   * The HTTP method this endpoint responds to.
+   *
+   * Set to HttpMethod::DELETE to indicate DELETE requests.
+   *
+   * @var HttpMethod
+   */
+  public HttpMethod $httpMethod = HttpMethod::DELETE;
+
+  /**
+   * The type of controller this attribute represents.
+   *
+   * Defaults to ControllerType::Endpoint to indicate that this
+   * attribute marks an actual endpoint method.
+   *
+   * @var ControllerType
+   */
   public ControllerType $controllerType = ControllerType::Endpoint;
 }
