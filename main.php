06987163e17446a796bf1955751a95e851bcc4bc
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2026-01-02 18:23:34 -0300

    Atualizar componentes

--- main.php
+++ main.php
@@ -1,9 +1,6 @@
 <?php
 
-use Websyspro\Core\Server\HttpModule;
 use Websyspro\Core\Server\HttpServer;
-use Websyspro\Core\Server\Request;
-use Websyspro\Core\Server\Response;
 use Websyspro\Test\Account\AccountModule;
 
 $httpServer = new HttpServer();
@@ -13,15 +10,4 @@ $httpServer->module(
   ]
 );
 
-
-// $httpServer->get( "/product/:productId{int}/detail/:message/test", function( Response $response, Request $request ) use ($httpServer) {
-//   $response->json( new HttpModule($httpServer, AccountModule::class) );
-// });
-
 $httpServer->listen();
-
-//$httpModule = new HttpModule($httpServer, AccountModule::class);
-
-// $httpServer->get( "/product/test", function( Response $response, Request $request ) {
-//   $response->json( $request->body() );
-// });
